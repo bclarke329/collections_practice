05d26d781c5f48512deedbf37a68f41e005a8855
bclarke329 <bmendoza329@hotmail.com>
2020-10-05 22:08:15 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+    array.sort
+end 
 
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort_by{|element| element.length}
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    return array 
+end 
+
+def reverse_array(array)
+    array.reverse()
+end
+
+def kesha_maker(array)
+    array.each do |string|
+        string[2] = "$"
+    end 
+end 
+
+def find_a(array)
+     array.select{|element| element.start_with?("a")}
+end 
+
+def sum_array(array)
+    array.inject(0){|sum,x| sum + x }
+end 
+
+def add_s(array)
+    array.each_with_index.collect do |element, index|
+        index == 1? element:element + "s"
+    end
+end 
